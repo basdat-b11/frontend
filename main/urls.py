20f6eb5ce58f9dea79775c2a950137490cee6f36
MRadhityaUtomo <radhityautomo26@gmail.com>
2024-04-28 02:05:18 +0700

    nambahin CRUD KELOLA ALBUM & SONG (ARTIST/SONGWRITER)

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from main.views import test_akun
+from main.views import *;
 
 app_name = 'main'
 
 #testing testing blom paham lanjutnya gmn wkwk
 urlpatterns = [
-    path('', test_akun, name='test_akun'), 
+    path('', my_view, name='my_view'),
+    path('create-album', create_album, name='create_album'), 
+    path('list-album', list_album, name='list_album'),
+    path('create-lagu', create_lagu, name='create_lagu'),
+    path('test', test_akun, name='test_akun')
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,35 @@ from django.http import HttpResponse
 import json
 
 # Create your views here.
+def create_album(request):
+    # Lakukan operasi atau logika yang diperlukan
+    context = {
+        'variable': 'nilai_variable'
+    }
+    return render(request, 'create_album.html', context)
+
+def list_album(request):
+    # Lakukan operasi atau logika yang diperlukan
+    context = {
+        'variable': 'nilai_variable'
+    }
+    return render(request, 'list_album.html', context)
+
+def my_view(request):
+    # Lakukan operasi atau logika yang diperlukan
+    context = {
+        'variable': 'nilai_variable'
+    }
+    return render(request, 'main.html', context)
+
+def create_lagu(request):
+    # Lakukan operasi atau logika yang diperlukan
+    context = {
+        'variable': 'nilai_variable'
+    }
+    return render(request, 'create_lagu.html', context)
+
+
 def test_akun(request):
     result = ""
     with connection.cursor() as cursor:
