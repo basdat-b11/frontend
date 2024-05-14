d0c114fa32361558a99c0304b94936291706dff4
jnathanie <jocelyn.nathanie@ui.ac.id>
2024-05-14 16:49:46 +0700

    selesai fitur play user playlist

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from main.views import test_akun
+from main.views import test_akun, playlist, song_detail, kelola_playlist
 
 app_name = 'main'
 
 #testing testing blom paham lanjutnya gmn wkwk
 urlpatterns = [
     path('', test_akun, name='test_akun'), 
+    path('kelola_playlist/', kelola_playlist, name='kelola_playlist'),
+    path('song_detail/', song_detail, name='song_detail'),
+    path('playlist/', playlist, name='playlist'),
 ]
