5abcc915fc88afbe068f25bb6c9f7548c22ece33
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-16 19:50:53 +0700

    test auth

--- main/urls.py
+++ main/urls.py
@@ -5,13 +5,10 @@ app_name = 'main'
 
 #testing testing blom paham lanjutnya gmn wkwk
 urlpatterns = [
-    path('', my_view, name='my_view'),
-    path('create-album', create_album, name='create_album'), 
-    path('list-album', list_album, name='list_album'),
-    path('list-album-label', list_album_label, name='list_album_label'),
-    path('create-lagu', create_lagu, name='create_lagu'),
-    path('cek-royalti', cek_royalti, name='cek_royalti'),
-    path('lagulist-album', lagulist_album, name='lagulist_album'),
-    path('lagulist-album-label', lagulist_album_label, name='lagulist_album_label'),
-    path('test', test_akun, name='test_akun')
+    path('', first_view, name='first_view'),
+    path('test', test_akun, name='test_akun'),
+    path('role-register-view/', role_register_view, name='role-register-view'),
+    path('register-pengguna/', register_pengguna, name='register-pengguna'),
+    #path('register-pengguna/', register_pengguna, name='register-pengguna'),
+    path('login/', login_user, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,64 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.db import connection
 from django.http import HttpResponse
+from django.contrib import messages  
 import json
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def create_album(request):
+def first_view(request):
     # Lakukan operasi atau logika yang diperlukan
     context = {
         'variable': 'nilai_variable'
     }
-    return render(request, 'create_album.html', context)
+    return render(request, 'LoginRegister.html', context)
 
-def list_album(request):
+def role_register_view(request):
     # Lakukan operasi atau logika yang diperlukan
     context = {
         'variable': 'nilai_variable'
     }
-    return render(request, 'list_album.html', context)
-
-def list_album_label(request):
-    # Lakukan operasi atau logika yang diperlukan
-    context = {
-        'variable': 'nilai_variable'
-    }
-    return render(request, 'list_album_label.html', context)
-
-def my_view(request):
-    # Lakukan operasi atau logika yang diperlukan
-    context = {
-        'variable': 'nilai_variable'
-    }
-    return render(request, 'main.html', context)
-
-def create_lagu(request):
-    # Lakukan operasi atau logika yang diperlukan
-    context = {
-        'variable': 'nilai_variable'
-    }
-    return render(request, 'create_lagu.html', context)
-
-def cek_royalti(request):
-    # Lakukan operasi atau logika yang diperlukan
-    context = {
-        'variable': 'nilai_variable'
-    }
-    return render(request, 'cek_royalti.html', context)
-
-def lagulist_album(request):
-    # Lakukan operasi atau logika yang diperlukan
-    context = {
-        'variable': 'nilai_variable'
-    }
-    return render(request, 'lagulist_album.html', context)
-
-def lagulist_album_label(request):
-    # Lakukan operasi atau logika yang diperlukan
-    context = {
-        'variable': 'nilai_variable'
-    }
-    return render(request, 'lagulist_album_label.html', context)
+    return render(request, 'RoleRegister.html', context)
 
 def test_akun(request):
     result = ""
@@ -69,3 +30,62 @@ def test_akun(request):
     
     print(result)
     return HttpResponse(result, content_type='application/json', status=200)
+
+def register_pengguna(request):
+    
+    is_verified = "f"
+    is_male = "0"
+    if request.method == "POST":
+        email = request.POST['inputEmail']
+        password = request.POST['inputPasswordl']
+        name = request.POST['inputName']
+        gender = request.POST['inputGender']
+        birth_place = request.POST['inputPlaceOfBirth']
+        birth_date = request.POST['inputDateOfBirth']
+        city = request.POST['inputCity']
+        role = request.POST['inputRole']
+        if (role != ""):
+            is_verified = "t"
+
+        if (gender == "Male"):
+            is_male = "1"
+
+        with connection.cursor() as cursor:
+            cursor.execute("Set search_path to marmut;")
+            cursor.execute("""
+                            INSERT INTO akun (
+                                email,
+                                password,
+                                nama,
+                                gender, 
+                                tempat_lahir,
+                                tanggal_lahir,
+                                is_verified,
+                                kota_asal
+                            )
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                            """, [email, password, name, is_male, birth_place, birth_date, is_verified, city])
+    return render(request, 'RegisterPengguna.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        name = request.POST.get('name')
+        gender = request.POST.get('gender')
+        birth_place = request.POST.get('birth_place')
+        birth_date = request.POST.get('birth_date')
+        city = request.POST.get('city')
+        role = request.POST.get('role')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'LoginPage.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
