33cd55afd91b06cca2c0ed3bae904f4877565fb7
Amanda Oktivia <143381263+amandaoktivia14@users.noreply.github.com>
2024-05-17 21:17:40 +0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,14 +7,6 @@ app_name = 'main'
 
 #testing testing blom paham lanjutnya gmn wkwk
 urlpatterns = [
-<<<<<<< HEAD
-    path('', first_view, name='first_view'),
-    path('test', test_akun, name='test_akun'),
-    path('role-register-view/', role_register_view, name='role-register-view'),
-    path('register-pengguna/', register_pengguna, name='register-pengguna'),
-    #path('register-pengguna/', register_pengguna, name='register-pengguna'),
-    path('login/', login_user, name='login'),
-=======
     path('', test_akun, name='test_akun'),
     path('kelolaalbum/', include('kelolaalbum.urls')),
     path('kelolaplaylist/', include('kelolaplaylist.urls')),
@@ -28,5 +20,4 @@ urlpatterns = [
     path('lagulist-album', lagulist_album, name='lagulist_album'),
     path('lagulist-album-label', lagulist_album_label, name='lagulist_album_label'),
     path('test', test_akun, name='test_akun')
->>>>>>> bdc8c87a0488f7c070a2d7c2e4121a5e08182fb4
 ]
