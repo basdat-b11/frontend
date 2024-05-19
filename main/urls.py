d0f8bbce63ea7159895eeeaec401f5a3b650d0bd
MRadhityaUtomo <radhityautomo26@gmail.com>
2024-05-19 22:17:56 +0700

    readded urlpatterns

--- main/urls.py
+++ main/urls.py
@@ -7,9 +7,19 @@ app_name = 'main'
 
 #testing testing blom paham lanjutnya gmn wkwk
 urlpatterns = [
+    path('songdetail/', include('songdetail.urls')),
+    path('playlist/', include('playlist.urls')),
+    # path('create-album', create_album, name='create_album'), 
+    # path('list-album', list_album, name='list_album'),
+    # path('list-album-label', list_album_label, name='list_album_label'),
+    # path('create-lagu', create_lagu, name='create_lagu'),
+    # path('cek-royalti', cek_royalti, name='cek_royalti'),
+    # path('lagulist-album', lagulist_album, name='lagulist_album'),
+    # path('    lagulist-album-label', lagulist_album_label, name='lagulist_album_label'),
     path('langganan-paket/', subscription_page, name='susbcription_page'),
     path('pembayaran-paket/', bayar_paket, name='bayar_paket'),
     path('riwayat-langganan/', riwayat_langganan, name='riwayat_langganan'),
     path('unduhan-lagu/', unduhan_lagu, name='unduhan_lagu'),
     path('cari', pencarian, name='pencarian'),
+    # path('lagulist-album-label', lagulist_album_label, name='lagulist_album_label'),
 ]
