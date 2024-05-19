3a80a8465e3f5bd347f29222b2c37f67aa427224
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-19 18:03:52 +0700

    implement chart

--- melihatchart/urls.py
+++ melihatchart/urls.py
@@ -5,8 +5,5 @@ app_name = 'melihatchart'
 
 urlpatterns = [
     path('', chart_list, name='chart_list'), 
-    path('daily-chart-detail', daily_chart_detail, name='daily_chart_detail'), 
-    path('weekly-chart-detail', weekly_chart_detail, name='weekly_chart_detail'), 
-    path('monthly-chart-detail', monthly_chart_detail, name='monthly_chart_detail'), 
-    path('yearly-chart-detail', yearly_chart_detail, name='yearly_chart_detail'), 
+    path('chart-detail/<str:id>/<str:tipe>', chart_detail, name='chart_detail'),
 ]
